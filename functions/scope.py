26632e43b51fe01cd90d411d701c4470195b4377
ZeeshanJ99 <88186084+ZeeshanJ99@users.noreply.github.com>
2021-08-11 11:13:43 +0100

    hangman v2

---
+++ functions/scope.py
@@ -0,0 +1,16 @@
+def square(num):
+    result = num ** 2
+    return result
+
+print(square(10))
+
+
+
+# LEGB rule
+# local
+# enclosed
+# global
+# built-in
+
+# local scope is stuff defined only for functions
+# we can define functions inside of another function
--- hangman/Beginning the game.py
+++ hangman/Beginning the game.py
@@ -2,8 +2,7 @@ import random
 from hangman_words import word_list
 
 
-def word():
-    return random.choice(word_list)
+word = random.choice(word_list)
 
 
 allowed_errors = 8
@@ -11,7 +10,7 @@ guesses = []
 done = False
 
 while not done:
-    for letter in word():
+    for letter in word:
         if letter.upper() in guesses:
             print(letter, end=" ")
         else:
@@ -20,19 +19,22 @@ while not done:
 
     guess = input(f"Allowed Errors left {allowed_errors}, Next Guess: ")
     guesses.append(guess.upper())
-    if guess.upper() not in word():
+    if guess.upper() not in word:
         allowed_errors -= 1
+    elif guess.upper() not in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+        print('Please enter a LETTER.')
         if allowed_errors == 0:
             break
 
     done = True
-    for letter in word():
+    for letter in word:
         if letter.upper() not in guesses:
             done = False
 
-    if done:
+if done:
         print(f"You found the word, Congratulations!!!")
-    elif():
-        print(f"Game Over, you failed the word was {word()}")
+else:
+        print(f"Game Over, you failed the word was {word}")
+
 
 
