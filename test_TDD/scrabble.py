4c23256ec5b83df68a65e4581da05f1b0d78dbbc
ZeeshanJ99 <88186084+ZeeshanJ99@users.noreply.github.com>
2021-08-12 21:18:55 +0100

    scrabble word calc sorted out

--- test_TDD/scrabble.py
+++ test_TDD/scrabble.py
@@ -20,28 +20,22 @@ class Scrabble:
             for two_point in two_point_values:
                 if letter == two_point:
                     self.point = self.point+2
-
-
-
-
-            for three self.point in three_point_values:
-                if (letter == three_point):
-                    point = point + 3
-            for four point in four_point_values:
-                if (letter == four_point):
-                    point = point + 4
-            for five point in five_point_values:
-                if (letter == five_point):
-                    point = point + 5
-            for eight point in eight_point_values:
-                if (letter == eight_point):
-                    point = point + 8
-            for ten point in ten_point_values:
-                if (letter == ten_point):
-                    point = point + 10
-            return point
-
-        return self.point
+            for three_point in three_point_values:
+                if letter == three_point:
+                    self.point = self.point+3
+            for four_point in four_point_values:
+                if letter == four_point:
+                    self.point = self.point + 4
+            for five_point in five_point_values:
+                if letter == five_point:
+                    self.point = self.point + 5
+            for eight_point in eight_point_values:
+                if letter == eight_point:
+                    self.point = self.point + 8
+            for ten_point in ten_point_values:
+                if letter == ten_point:
+                    self.point = self.point + 10
+            return self.point
 
     # def valid_word(self, letter):
     #     if letter in self.word:
